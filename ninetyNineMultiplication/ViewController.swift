49426a1762569be68615176de6c1508b5b861e82
casper41807 <casper41807@gmail.com>
2022-04-13 18:51:29 +0800

    Initial Commit

---
+++ ninetyNineMultiplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ninetyNineMultiplication
+//
+//  Created by 陳秉軒 on 2022/4/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
